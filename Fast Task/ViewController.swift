b58a57c48a90260ce6f36287a5f9cc04fb07f501
Isaac Ansumana <isaac.ansumana@gmail.com>
2019-07-05 19:31:05 -0700

    Initial Commit

---
+++ Fast Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fast Task
+//
+//  Created by Trill Isaac on 7/5/19.
+//  Copyright © 2019 Isaac Ansumana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
